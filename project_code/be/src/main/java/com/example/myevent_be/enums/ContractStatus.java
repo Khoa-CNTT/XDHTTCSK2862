a8e1dfe9fb291495707e3e8f2b5984cdbd4cf160
Hoang Long <phamngochoanglong48@gmail.com>
2025-04-12 13:29:47 +0700

    update: Contract API

--- project_code/be/src/main/java/com/example/myevent_be/enums/ContractStatus.java
+++ project_code/be/src/main/java/com/example/myevent_be/enums/ContractStatus.java
@@ -1,4 +1,12 @@
 package com.example.myevent_be.enums;
 
 public enum ContractStatus {
+    DRAFT,    // Nháp
+    DEPOSITED,   // đã đặt cọc
+    PROGRESS,   //  thực hiện
+    PENDING,    // Đang chờ
+    PENDING_PAYMENT,  // Chờ thanh toán
+    COMPLETED,// Hoàn thành
+    CANCELLED,//Đã huỷ
+    CANCELLED_BY_ADMIN// Đã hủy bởi admin
 }

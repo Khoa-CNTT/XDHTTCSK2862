85a67ddcfcf9e2b0b7399fabe9d7c39290bf0a63
Khanh Huyen <ngohuyen.280703@gmail.com>
2025-05-14 13:26:34 +0700

    fix device, location

--- project_code/be/src/main/java/com/example/myevent_be/entity/Location.java
+++ project_code/be/src/main/java/com/example/myevent_be/entity/Location.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Nationalized;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
@@ -25,11 +26,14 @@ public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     String id;
-
+    @Nationalized
     String name;
+    @Nationalized
     String description;
     String image;
     BigDecimal hourly_rental_fee;
+    @Nationalized
+    String address;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
